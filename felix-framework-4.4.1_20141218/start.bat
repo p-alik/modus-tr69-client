01def5e568908475cc9d7bcf959fb86536bd6bcb
achazalet <achazalet>
2014-12-18 09:38:19 +0000

    Update READ_ME.txt, and add a turn-key felix-framework-4.4.1_20141218.

---
+++ felix-framework-4.4.1_20141218/start.bat
@@ -0,0 +1,2 @@
+
+java -Dorg.osgi.service.enocean.loglevel.debug=true -Dorg.osgi.service.enocean.loglevel.warn=true -Dorg.osgi.service.enocean.loglevel.info=true -Dorg.osgi.service.enocean.loglevel.error=true -Dmbs.comm.debug=true -jar ./bin/felix.jar
